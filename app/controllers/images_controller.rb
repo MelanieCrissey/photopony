98496d7ac475a711842f667897589a68873d7f56
Melanie Crissey <melaniecrissey@gmail.com>
2014-05-28 20:31:45 -0400

    Deleting Font Awesome

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:name, :rating, :feels, :image)
+      params.require(:image).permit(:name, :feels, :image)
     end
 end
